5e5fad3443399d95710595b9cde1aaf36279041a
Huajun Gu <GU-GAMING@GU-GAMING.localdomain>
2018-04-28 12:46:47 -0400

    added Keras NN based approach

--- PredictionRL.py
+++ PredictionRL.py
@@ -40,7 +40,7 @@ class BlackjackEnv():
             return 100
         elif finished and result == Blackjack.RESULT["push"]:
             # Push
-            return -20
+            return -25
         elif not finished:
             # Still in the middle of a hand
             return 0
@@ -65,7 +65,7 @@ def Q_learning_with_epsilon_greedy():
     # Set up some hyperparameters
     q_table = np.zeros((21, 11, 2))
     y = 0.95
-    eps = 0.5
+    eps = 0.3
     lr = 0.8
     decay_factor = 0.999
 
@@ -92,10 +92,10 @@ def Q_learning_with_epsilon_greedy():
             new_s_cards, r, done, _ = env.step(a)
             if done:
                 new_s = cards_to_state(*new_s_cards, only_one_dealer_card=False)  # This is just to see if we won or not
-                q_table[s[0], s[1], a] += r + lr * (- q_table[s[0], s[1], a])
+                q_table[s[0], s[1], a] += lr * (r - q_table[s[0], s[1], a])
             else:
                 new_s = cards_to_state(*new_s_cards)
-                q_table[s[0], s[1], a] += r + lr * (y * np.max(q_table[new_s[0], new_s[1], :]) - q_table[s[0], s[1], a])
+                q_table[s[0], s[1], a] += lr * (r + y * np.max(q_table[new_s[0], new_s[1], :]) - q_table[s[0], s[1], a])
             s = new_s
 
     # Save the q_table to a csv
@@ -117,22 +117,19 @@ def Q_learning_with_epsilon_greedy():
         if i % 100 == 0:
             print("Evaluation episode {} of {}".format(i + 1, num_episodes_evaluating))
 
-        # Update hyperparameters
-        eps *= decay_factor
-
         # Deal next hand
-        s = cards_to_state(*env.deal())
+        s = cards_to_state(*env_eval.deal())
         done = False
 
         while not done:
             # select the action with highest cumulative reward
-            if np.random.random() < eps or np.sum(q_table[s[0], s[1], :]) == 0:
+            if np.sum(q_table[s[0], s[1], :]) == 0:
                 a = np.random.randint(0, 2)
             else:
                 a = np.argmax(q_table[s[0], s[1], :])
 
             # Take the step based on the learned Q-table
-            new_s_cards, r, done, extra = env.step(a)
+            new_s_cards, r, done, extra = env_eval.step(a)
             result = extra[0]
             if done:
                 wins += 1 if result == Blackjack.RESULT["win"] else 0
@@ -144,63 +141,107 @@ def Q_learning_with_epsilon_greedy():
     return wins, pushes, loses
 
 
-def usingKeras():
+def learning_with_keras():
     import keras.layers
 
     # Convert given state as a tuple of dealer_cards and player_cards into a one-hot matrix
-    def cards_to_state(dealer_cards, player_cards):
-        s = np.zeros([1, 22])
-        s[0, min(21, Blackjack.get_card_value(player_cards) - 1)] = 1
+    def cards_to_state(dealer_cards, player_cards, only_one_dealer_card=True):
+        dealer_cards_interested_in = [dealer_cards[0]] if only_one_dealer_card else dealer_cards
+        player_value = Blackjack.get_card_value(player_cards)
+        dealer_value = Blackjack.get_card_value(dealer_cards_interested_in)
+        s = np.zeros([1, 21 * 11])
+        s[0, (player_value - 1) * 11 + (dealer_value - 1)] = 1
         return s
 
 
     # Keras model for the weights
     model = keras.models.Sequential()
-    model.add(keras.layers.InputLayer(batch_input_shape=(1, 22)))
-    model.add(keras.layers.Dense(22 * 2, activation='sigmoid'))
+    model.add(keras.layers.InputLayer(batch_input_shape=(1, 21 * 11)))
+    model.add(keras.layers.Dense(21 * 11 * 2, activation='sigmoid'))
     model.add(keras.layers.Dense(2, activation='linear'))
     model.compile(loss='mse', optimizer='adam', metrics=['mae'])
 
     # Gym model that we will use to simulate stuff
     env = BlackjackEnv()
 
-    # Number of iterations
-    num_episodes = 10000
-
+    # Set up some hyperparameters
     y = 0.95
     eps = 0.5
     decay_factor = 0.999
-    r_sum_list = []
-    for i in range(num_episodes):
-        s = env.reset()
-        eps *= decay_factor
+
+    # Number of iterations
+    num_episodes_learning = 100000
+    num_episodes_evaluating = 10000
+
+    env.reset()
+    for i in range(num_episodes_learning):
         if i % 100 == 0:
-            print("Episode {} of {}".format(i + 1, num_episodes))
+            print("Learning episode {} of {}".format(i + 1, num_episodes_learning))
+
+        # Update hyperparameters
+        eps *= decay_factor
+
+        # Deal next hand
+        s = cards_to_state(*env.deal())
         done = False
-        r_sum = 0
+
         while not done:
+            # select the action with highest cumulative reward
+            predicted_for_s = model.predict(s)
             if np.random.random() < eps:
                 a = np.random.randint(0, 2)
             else:
-                a = np.argmax(model.predict(cards_to_state(*s)))
-                print(a)
-            new_s, r, done, _ = env.step(a)
-            target = r + y * np.max(model.predict(cards_to_state(*new_s)))
-            target_vec = model.predict(cards_to_state(*s))[0]
+                a = np.argmax(predicted_for_s)
+
+            # Take the step and then update the q_table
+            new_s_cards, r, done, _ = env.step(a)
+            if done:
+                new_s = None
+                target = r
+                target_vec = predicted_for_s[0]
+                target_vec[a] = target
+            else:
+                new_s = cards_to_state(*new_s_cards)
+                target = r + y * np.max(model.predict(new_s))
+            target_vec = predicted_for_s[0]
             target_vec[a] = target
-            model.fit(cards_to_state(*s), target_vec.reshape(-1, 2), epochs=1, verbose=0)
+            model.fit(s, target_vec.reshape(-1, 2), epochs=1, verbose=0)
             s = new_s
-            r_sum += r
-        if r_sum == 45:
-            print(s)
-        r_sum_list.append(r_sum)
 
-    with open("test.csv", "w") as file:
-        for r_sum in r_sum_list:
-            file.write("{}\n".format(r_sum))
+    # Evaluate how good this q_table is
+    env_eval = BlackjackEnv()
+    env_eval.seed("eval")
+    env_eval.reset()
 
-    # plt.plot(r_sum_list)
-    # plt.show()
+    wins = 0
+    pushes = 0
+    loses = 0
 
+    for i in range(num_episodes_evaluating):
+        if i % 100 == 0:
+            print("Evaluation episode {} of {}".format(i + 1, num_episodes_evaluating))
 
-print(Q_learning_with_epsilon_greedy())
+        # Update hyperparameters
+        eps *= decay_factor
+
+        # Deal next hand
+        s = cards_to_state(*env_eval.deal())
+        done = False
+
+        while not done:
+            a = np.argmax(model.predict(s))
+
+            # Take the step based on the learned Q-table
+            new_s_cards, r, done, extra = env_eval.step(a)
+            result = extra[0]
+            if done:
+                wins += 1 if result == Blackjack.RESULT["win"] else 0
+                pushes += 1 if result == Blackjack.RESULT["push"] else 0
+                loses += 1 if result == Blackjack.RESULT["lose"] else 0
+            else:
+                s = cards_to_state(*new_s_cards)
+
+    return wins, pushes, loses
+
+# print(Q_learning_with_epsilon_greedy())
+print(learning_with_keras())
